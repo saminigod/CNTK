872544a870a4d3c0da067e39d6f9de602e07ad6a
Mark Hillebrand <mahilleb@microsoft.com>
2017-01-10 10:39:24 +0100

    Make Jupyter Notebook tests device-aware

--- Tests/EndToEndTests/CNTKv2Python/Examples/conftest.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/conftest.py
@@ -57,11 +57,24 @@ def pytest_generate_tests(metafunc):
 
 @pytest.fixture(scope='module')
 def nb(tmpdir_factory, request, device_id):
-    # TODO we need a way to inject device_id into the notebook
     import nbformat
     import os
     import subprocess
+    from cntk.ops.tests.ops_test_utils import cntk_device
+    from cntk.cntk_py import DeviceKind_GPU
     inPath = getattr(request.module, "notebook")
+    deviceIdsToRun = [-1, 0]
+    try:
+        deviceIdsToRun = getattr(request.module, "notebook_deviceIdsToRun")
+    except AttributeError:
+        pass
+    # Pass along device_id type to child process
+    if cntk_device(device_id).type() == DeviceKind_GPU:
+        os.environ['TEST_DEVICE'] = 'gpu'
+    else:
+        os.environ['TEST_DEVICE'] = 'cpu'
+    if not device_id in deviceIdsToRun:
+        pytest.skip('test not configured to run on device ID {0}'.format(device_id))
     outPath = str(tmpdir_factory.mktemp('notebook').join('out.ipynb'))
     assert os.path.isfile(inPath)
     kernel_name_opt = "--ExecutePreprocessor.kernel_name=python%d" % (sys.version_info[0])
